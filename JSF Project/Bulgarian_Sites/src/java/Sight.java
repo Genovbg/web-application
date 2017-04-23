0ec700c9a9b2227ef364a2ed7c08bf7e83814617
Stefan Genov <stefan.genov@mail.bg>
2017-04-23 21:56:41 +0300

    Add files via upload

---
+++ JSF Project/Bulgarian_Sites/src/java/Sight.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stefan
+ */
+public class Sight {
+    String image;
+    String name;
+    String descriptions;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public Sight(String image, String name, String descriptions) {
+        this.setImage(image);
+        this.setName(name);
+        this.setDescriptions(descriptions);
+    }
+    
+    
+    
+}
